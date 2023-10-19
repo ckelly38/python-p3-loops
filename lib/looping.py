234ca04d83734964b4b03ffe8dc316a871960f33
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-10-19 03:27:42 -0600

    range does not include the number given

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,30 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    # normally for something like counting down, I would use a for loop.
+    # I am forced to use a while loop.
+    i = 10;
+    while((i > 0) and (i < 10 or i == 10)):
+        print(i);
+        i -= 1;
+    print("Happy New Year!");
+    return None;
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    if (int_list == None): return None;
+    elif (len(int_list) < 1): return [];
+    else: pass;
+    return [num * num for num in int_list];
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(100):
+        num = i + 1;
+        if (num % 3 == 0): print("Fizz", end="");
+        else: pass;
+        if (num % 5 == 0): print("Buzz", end="");
+        else: pass;
+        if ((num % 3 == 0) or (num % 5 == 0)): print("");
+        else: print(num);
+    return None;
